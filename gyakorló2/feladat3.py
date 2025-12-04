ac6fcd9ae9deb3aee10c0030c4acb27d3c68a3f6
FZoltan40 <farkasz@kkszki.hu>
2025-12-04 16:55:06 +0100

    Gyakorlo II feladat3 is ready.

---
+++ gyakorló2/feladat3.py
@@ -0,0 +1,20 @@
+"""
+3. feladat – Prímszám ellenőrzése
+Döntsd el egy számról, hogy prímszám-e (szelekció + iteráció).
+"""
+
+n = int(input("Kérek egy egész számot:"))
+prim = True
+
+if n < 2:
+    prim = False
+else:
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            prim = False
+            break
+
+if prim == True:
+    print("A megadott szám prím szám.")
+else:
+     print("A megadott szám nem prím szám.")
